49034a86ee99973c11ae51b4ce4fa83922969473
OdilonJulio <odilonjulio@ufrn.edu.br>
2019-06-14 01:45:47 -0300

    caldas prontas

--- IMD0606 - PROJETO EM BIOINFORMÁTICA - T01 (2019.1)/script_CD274_Expression.Gene(com câncer).R
+++ IMD0606 - PROJETO EM BIOINFORMÁTICA - T01 (2019.1)/script_CD274_Expression.Gene(com câncer).R
@@ -18,6 +18,9 @@ SkinExpGen <- arrange(SkinExpGen, desc(Exp_Z_CD274))
 head(LungExpGen)
 head(SkinExpGen)
 
+dim(SkinExpGen)
+dim(LungExpGen)
+
 max(LungExpGen$Exp_Z_CD274)
 max(SkinExpGen$Exp_Z_CD274)
 
@@ -28,39 +31,56 @@ tail(SkinExpGen)
 min(LungExpGen$Exp_Z_CD274)
 min(SkinExpGen$Exp_Z_CD274)
 
-# Histogramas e outros gráficos
-hist(LungExpGen$Exp_Z_CD274, nclass=50)
-hist(SkinExpGen$Exp_Z_CD274, nclass=50)
-
-plot(dnorm(LungExpGen$Exp_Z_CD274))
-
-hist(LungExpGen$Exp_Z_CD274, prob = T, nclass=50)
-hist(SkinExpGen$Exp_Z_CD274, prob = T, nclass=50)
-
-#x <- seq(min(LungExpGen$Exp_Z_CD274), max(LungExpGen$Exp_Z_CD274), 0.1)
-plot(LungExpGen)
-
-# Médias
-mean(LungExpGen$Exp_Z_CD274)
-mean(SkinExpGen$Exp_Z_CD274)
-
-# Desvio Padrão
+# Médidas de dispersão e de tendência central.
 sd(LungExpGen$Exp_Z_CD274)
 sd(SkinExpGen$Exp_Z_CD274)
 
-#LungExpGenMax <- head(LungExpGen,100)
-#LungExpGenMin <- tail(LungExpGen,100)
+mean(LungExpGen$Exp_Z_CD274)
+mean(SkinExpGen$Exp_Z_CD274)
 
-dim(SkinExpGen)
-dim(LungExpGen)
+var(LungExpGen$Exp_Z_CD274)
+var(SkinExpGen$Exp_Z_CD274)
+
+median(LungExpGen$Exp_Z_CD274)
+median(SkinExpGen$Exp_Z_CD274)
+
+# Histogramas e outros gráficos
+par(mfrow = c(1, 2)) # Possibilita visualizar vários plots ao mesmo tempo.
+hist(LungExpGen$Exp_Z_CD274, nclass=500, main="Lung")
+hist(SkinExpGen$Exp_Z_CD274, nclass=500, main="Skin")
+
+qqnorm(LungExpGen$Exp_Z_CD274, main="Lung")
+qqline(LungExpGen$Exp_Z_CD274, col='red')
+qqnorm(SkinExpGen$Exp_Z_CD274, main="Skin")
+qqline(SkinExpGen$Exp_Z_CD274, col='red')
+
+plot(LungExpGen, main="Lung")
+plot(SkinExpGen, main="Skin")
+
+hist(LungExpGen$Exp_Z_CD274, prob = T, nclass=50, main="Lung")
+hist(SkinExpGen$Exp_Z_CD274, prob = T, nclass=50, main="Skin")
+
+par(mfrow = c(1, 1))
 
 ## Filtros
+## Separando os indivíduos com maior e menor expressão em 
+## 2,5% em cada uma das extremidades.
 
-# Calda dos muitos expressos
-LungCaldaMax <- filter(LungExpGen, LungExpGen$Exp_Z_CD274 >= (sd(LungExpGen$Exp_Z_CD274)/2))
-min(LungCaldaMax$Exp_Z_CD274)
+# Superexpressos.
+LungExpGenMax <- head(LungExpGen,0.025*length(LungExpGen$Exp_Z_CD274))
+SkinExpGenMax <- head(SkinExpGen,0.025*length(SkinExpGen$Exp_Z_CD274))
 
-# Calda dos poucos expressos
-LungCaldaMin <- filter(LungExpGen, LungExpGen$Exp_Z_CD274 <= -(sd(LungExpGen$Exp_Z_CD274)/2))
-max(LungCaldaMin$Exp_Z_CD274)
+# Pouco expressos.
+LungExpGenMin <- tail(LungExpGen,0.025*length(LungExpGen$Exp_Z_CD274))
+SkinExpGenMin <- tail(SkinExpGen,0.025*length(SkinExpGen$Exp_Z_CD274))
 
+# Avaliando distâncias.
+# Distância entre o menor dos maiores e o maior dos menores - Lung
+abs(min(LungExpGenMax$Exp_Z_CD274)/max(LungExpGenMin$Exp_Z_CD274))
+
+# Distância entre o menor dos maiores e o maior dos menores - Skin
+abs(min(SkinExpGenMax$Exp_Z_CD274)/max(SkinExpGenMin$Exp_Z_CD274))
+
+
+
+## COLETAR A SOBREVIDA DESSES INDIVÍDUOS
