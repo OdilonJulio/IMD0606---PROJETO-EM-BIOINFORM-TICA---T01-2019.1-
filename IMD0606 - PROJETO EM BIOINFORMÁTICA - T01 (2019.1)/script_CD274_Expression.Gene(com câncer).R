507b07af38434ebf85c6b8258ad9cea239428cd9
OdilonJulio <odilonjulio@ufrn.edu.br>
2019-06-14 20:17:11 -0300

    criando o Kaplan-Meyer

--- IMD0606 - PROJETO EM BIOINFORMÁTICA - T01 (2019.1)/script_CD274_Expression.Gene(com câncer).R
+++ IMD0606 - PROJETO EM BIOINFORMÁTICA - T01 (2019.1)/script_CD274_Expression.Gene(com câncer).R
@@ -1,14 +1,19 @@
 
+
+
+
 library(ggplot2)
+library(swirl)
 library(tidyr)
 library(dplyr)
 library(lubridate)
+library(survival)
 
 setwd("~/IMD0606---PROJETO-EM-BIOINFORM-TICA---T01-2019.1-/IMD0606 - PROJETO EM BIOINFORMÁTICA - T01 (2019.1)")
 
 # Expressão Gênica normalizada (Z-Score) do CD274 em tecidos Skin com câncer (Melanoma).
-SkinExpGen <- read.csv('cBioPortal_data_SKIN_mRNA_Expression z-Scores(RNA Seq).csv')
-LungExpGen <- read.csv('cBioPortal_data_LUNG_mRNA_Expression z-Scores(RNA Seq).csv')
+SkinExpGen <- read.csv('cBioPortal_data_SKIN_mRNA_Expression z-Scores(RNA Seq).csv',  dec = ",")
+LungExpGen <- read.csv('cBioPortal_data_LUNG_mRNA_Expression z-Scores(RNA Seq).csv',  dec = ",")
 
 # Ordenando pela Expressão Gênica em Z-Score
 LungExpGen <- arrange(LungExpGen, desc(Exp_Z_CD274))
@@ -23,6 +28,9 @@ dim(LungExpGen)
 
 max(LungExpGen$Exp_Z_CD274)
 max(SkinExpGen$Exp_Z_CD274)
+
+min(LungExpGen$Exp_Z_CD274)
+min(SkinExpGen$Exp_Z_CD274)
 
 # Mínimas Expressões Relativas
 tail(LungExpGen)
@@ -54,33 +62,58 @@ qqline(LungExpGen$Exp_Z_CD274, col='red')
 qqnorm(SkinExpGen$Exp_Z_CD274, main="Skin")
 qqline(SkinExpGen$Exp_Z_CD274, col='red')
 
-plot(LungExpGen, main="Lung")
-plot(SkinExpGen, main="Skin")
 
 hist(LungExpGen$Exp_Z_CD274, prob = T, nclass=50, main="Lung")
 hist(SkinExpGen$Exp_Z_CD274, prob = T, nclass=50, main="Skin")
 
 par(mfrow = c(1, 1))
 
-## Filtros
-## Separando os indivíduos com maior e menor expressão em 
-## 2,5% em cada uma das extremidades.
-
-# Superexpressos.
-LungExpGenMax <- head(LungExpGen,0.025*length(LungExpGen$Exp_Z_CD274))
-SkinExpGenMax <- head(SkinExpGen,0.025*length(SkinExpGen$Exp_Z_CD274))
-
-# Pouco expressos.
-LungExpGenMin <- tail(LungExpGen,0.025*length(LungExpGen$Exp_Z_CD274))
-SkinExpGenMin <- tail(SkinExpGen,0.025*length(SkinExpGen$Exp_Z_CD274))
-
-# Avaliando distâncias.
-# Distância entre o menor dos maiores e o maior dos menores - Lung
-abs(min(LungExpGenMax$Exp_Z_CD274)/max(LungExpGenMin$Exp_Z_CD274))
-
-# Distância entre o menor dos maiores e o maior dos menores - Skin
-abs(min(SkinExpGenMax$Exp_Z_CD274)/max(SkinExpGenMin$Exp_Z_CD274))
+### Filtros
+# Selecionando pacientes ainda com câncer.
+# Lung
+LungExpGenCa <- filter(LungExpGen, LungExpGen$CANCER == "Recurred/Progressed")
+dim(LungExpGenCa)
+# Skin
+SkinExpGenCa <- filter(SkinExpGen, SkinExpGen$CANCER == "Recurred/Progressed")
+dim(SkinExpGenCa)
 
 
+## Separando os indivíduos com maior e menor expressão.
+# Maior expressão
+LungExpGenCaMax <- filter(LungExpGenCa, LungExpGenCa$Exp_Z_CD274 > median(LungExpGenCa$Exp_Z_CD274))
+SkinExpGenCaMax <- filter(SkinExpGenCa, SkinExpGenCa$Exp_Z_CD274 > median(SkinExpGenCa$Exp_Z_CD274))
+# Menor expressão
+LungExpGenCaMin <- filter(LungExpGenCa, LungExpGenCa$Exp_Z_CD274 < median(LungExpGenCa$Exp_Z_CD274))
+SkinExpGenCaMin <- filter(SkinExpGenCa, SkinExpGenCa$Exp_Z_CD274 < median(SkinExpGenCa$Exp_Z_CD274))
+  
+# Superexpressos (principais pacientes)
+summary(LungExpGenCaMax)
+summary(SkinExpGenCaMax)
 
-## COLETAR A SOBREVIDA DESSES INDIVÍDUOS
+# Pouco expressos (principais pacientes)
+summary(LungExpGenCaMin)
+summary(SkinExpGenCaMin)
+
+# Tratando a coluna SURVIVAL
+LungExpGenCaMax <- mutate(LungExpGenCaMax, SURVIVAL=as.integer(SURVIVAL))
+LungExpGenCaMin <- mutate(LungExpGenCaMin, SURVIVAL=as.integer(SURVIVAL))
+SkinExpGenCaMax <- mutate(SkinExpGenCaMax, SURVIVAL=as.integer(SURVIVAL))
+SkinExpGenCaMin <- mutate(SkinExpGenCaMin, SURVIVAL=as.integer(SURVIVAL))
+
+LungExpGenCaMax <- arrange(LungExpGenCaMax, desc(SURVIVAL))
+LungExpGenCaMin <- arrange(LungExpGenCaMin, desc(SURVIVAL))
+SkinExpGenCaMax <- arrange(SkinExpGenCaMax, desc(SURVIVAL))
+SkinExpGenCaMin <- arrange(SkinExpGenCaMin, desc(SURVIVAL))
+
+## Survival Overall
+SurvLungMax <- select(LungExpGenCaMax, SURVIVAL)
+SurvLungMax <- mutate(SurvLungMax, Calda = "Superexpresso"); SurvLungMax
+SurvLungMin <- select(LungExpGenCaMin, SURVIVAL)
+SurvLungMin <- mutate(SurvLungMin, Calda = "Subexpresso"); SurvLungMin
+
+# SurvLung <- mutate(SurvLungMax, SURVIVALMin = SurvLungMin$SURVIVALMin); SurvLung
+SurvLungMerge <- merge(SurvLungMax, SurvLungMin, all = TRUE); SurvLungMerge
+
+fit <- survfit(Surv(SURVIVAL) ~ Calda, data = SurvLungMerge)
+
+autoplot(fit)
